f9e2e38e4b6d299fcebcd56fb7e8cc5dfc27ac09
d0rbu <hacperu2010@gmail.com>
2025-03-23 23:05:02 -0700

    add parameterized hyperplane

--- scenes/ivt_proof.py
+++ scenes/ivt_proof.py
@@ -1,8 +1,7 @@
 from typing import Self
 
 import numpy as np
-from manim import BLUE, RED, ImageMobject, MovingCameraScene, Circle, ValueTracker, always_redraw, FadeIn, VGroup, WHITE, DR, UL, Line, PI
-from PIL import Image
+from manim import BLUE, RED, ImageMobject, MovingCameraScene, Circle, ValueTracker, always_redraw, FadeIn, VGroup, WHITE, DR, UL, Line, PI, Arrow
 from focus_ireland import FocusIreland
 
 
@@ -33,7 +32,21 @@ class IVTProof(MovingCameraScene):
         theta = ValueTracker(0)
         # draw a circle thats always on the bottom right of the screen
         angle_indicator = always_redraw(lambda: self.draw_angle_circle(theta))
-        self.add(angle_indicator)
+        self.play(FadeIn(angle_indicator))
+        self.play(theta.animate.set_value(PI / 8), run_time=0.2)
+        self.play(theta.animate.set_value(-PI / 8), run_time=0.4)
+        self.play(theta.animate.set_value(0), run_time=0.2)
+
+        self.wait(2)
+
+        origin = self.camera.frame.get_center()
+        bias = ValueTracker(0)
+        hyperplane = always_redraw(lambda: self.draw_hyperplane(origin, theta, bias, self.camera.frame.get_width()))
+
+        self.play(FadeIn(hyperplane))
+        self.play(theta.animate.set_value(PI / 16), run_time=0.2)
+        self.play(theta.animate.set_value(-PI / 16), run_time=0.4)
+        self.play(theta.animate.set_value(0), run_time=0.2)
 
         self.wait(5)
 
@@ -51,3 +64,17 @@ class IVTProof(MovingCameraScene):
         line = Line(start=circle.get_center(), end=circle.get_center() + angle_vector, color=WHITE)
 
         return VGroup(circle, line)
+
+    def draw_hyperplane(self: Self, origin: np.ndarray, theta: ValueTracker, bias: ValueTracker, camera_width: float) -> VGroup:
+        angle_vector = np.array([np.cos(theta.get_value()), np.sin(theta.get_value()), 0])
+        angle_normal_vector = np.array([-np.sin(theta.get_value()), np.cos(theta.get_value()), 0])
+        bias_vector = angle_vector * bias.get_value()
+
+        center = origin + bias_vector
+
+        LINE_RADIUS = 10
+        ARROW_LENGTH = 0.07
+        line = Line(start=center - LINE_RADIUS * angle_normal_vector, end=center + LINE_RADIUS * angle_normal_vector, color=WHITE)
+        arrow = Arrow(start=center, end=center + ARROW_LENGTH * camera_width * angle_vector, buff=0, color=WHITE)
+
+        return VGroup(line, arrow)
